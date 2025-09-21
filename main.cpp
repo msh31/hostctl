1430d14b7ada843ca5ddb12dc620f4f31730891b
Marco <180540729+msh31@users.noreply.github.com>
2025-09-21 20:18:17 +0200

    feat: add imgui demo window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
+#include "imgui.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_glfw.h"
+
+
 int main()
 {
     if(!glfwInit()) {
@@ -15,14 +20,27 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // to make macOS happy; should not be needed
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // no old OpenGL 
 
+// window creation    
     GLFWwindow* window;
     window = glfwCreateWindow( 1024, 768, "ImGui loader", NULL, NULL);
-
+    
     if(window == NULL) {
         fprintf(stderr, "Failed to open GLFW window. OpenGL 3.3 support is required!\n");
         glfwTerminate();
         return -1;
     }
+    glfwMakeContextCurrent(window);
+    
+//imgui context creation, flags & setting platform/render backends
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);          // Second param install_callback=true will install GLFW callbacks and chain to existing ones.
+    ImGui_ImplOpenGL3_Init();
 
 //main loop
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
@@ -30,9 +48,16 @@ int main()
         // can cause flickering
         glClear(GL_COLOR_BUFFER_BIT);
 
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        ImGui::ShowDemoWindow();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(window);
         glfwPollEvents();
-
     }
     while( 
         glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
@@ -40,6 +65,9 @@ int main()
     );
 
     std::cout << "goodbye!";
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
     glfwDestroyWindow(window);    
     glfwTerminate();
     return 0;
